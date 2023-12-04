f14f2e6f5f57cb28a6adb5d3325a70ced5358f07
GerardoFernandez7 <101151869+GerardoFernandez7@users.noreply.github.com>
2023-12-04 11:49:30 -0600

    Juego ahorcado

---
+++ ahorcado.py
@@ -0,0 +1,88 @@
+# Gerardo Fernandez
+# Parte 1
+
+import random
+import turtle
+import dibujos_turtle
+
+# Configuración de la ventana de Turtle
+turtle.Screen()
+turtle.hideturtle()
+
+# Declaración de variables
+palabras = ['programacion', 'computadora', 'teclado', 'raton', 'monitor']
+palabra = random.choice(palabras)
+max_intentos = 7
+adivinadas = ['_' for _ in palabra]
+incorrectas = []
+# Registraremos si cada parte del muneco ha sido dibujada
+partes_dibujadas = [False, False, False, False, False, False, False]
+
+# Funciones
+def dibujar_muneco(intentos):
+    if intentos >= 1 and not partes_dibujadas[0]:   # Palo
+        turtle.lt(45)
+        dibujos_turtle.linea(100, -150, -240, 'black')
+        turtle.rt(90)
+        dibujos_turtle.linea(100, -80, -170, 'black' )
+        turtle.lt(135)
+        dibujos_turtle.linea(350, -80, -170, 'black')
+        turtle.rt(90)
+        dibujos_turtle.linea(200, -80, 180, 'black')
+        partes_dibujadas[0] = True
+    if intentos >= 2 and not partes_dibujadas[1]:  # Cabeza
+        dibujos_turtle.circulo(50, 120, 80, 'white')
+        partes_dibujadas[1] = True
+    if intentos >= 3 and not partes_dibujadas[2]:  # Torzo
+        turtle.rt(90)
+        dibujos_turtle.linea(180, 120, 80, 'black' )
+        partes_dibujadas[2] = True
+    if intentos >= 4 and not partes_dibujadas[3]:  # Pierna
+        turtle.rt(45)
+        dibujos_turtle.linea(100, 120, -100, 'black')
+        partes_dibujadas[3] = True
+    if intentos >= 5 and not partes_dibujadas[4]: # Pierna
+        turtle.lt(90)
+        dibujos_turtle.linea(100, 120, -100, 'black')
+        partes_dibujadas[4] = True
+    if intentos >= 6 and not partes_dibujadas[5]: # Brazo
+        turtle.lt(90)
+        dibujos_turtle.linea(100, 120, -25, 'black' )
+        partes_dibujadas[5] = True
+    if intentos >= 7 and not partes_dibujadas[6]: # Brazo
+        turtle.rt(-90)
+        dibujos_turtle.linea(100, 120, -25, 'black' )
+
+# Bucle principal del juego
+print("### BIENVENIDO ###")
+while True:
+    opcion = input("¿Qué desea hacer?\n1. Jugar\n2. Salir\n")
+    if opcion == '1':
+        intentos = 0
+        acertadas = 0
+        adivinadas = ['_' for _ in palabra]
+        incorrectas = []
+        while intentos < max_intentos and acertadas < len(palabra):
+            letra = input(f'Adivina una letra de la palabra: {" ".join(adivinadas)}\n')
+            if letra in palabra:
+                for i, letra_palabra in enumerate(palabra):
+                    if letra_palabra == letra:
+                        adivinadas[i] = letra
+                acertadas += 1
+            else:
+                incorrectas.append(letra)
+                intentos += 1
+                dibujar_muneco(intentos)
+                print(f'Letras incorrectas: {", ".join(incorrectas)}')
+
+        if acertadas == len(palabra):
+            print(f'¡Ganaste! La palabra era "{palabra}".')
+            break
+        else:
+            print(f'¡Perdiste! La palabra era "{palabra}".')
+            break
+    elif opcion == '2':
+        print("¡Hasta luego!")
+        break
+    else:
+        print("Seleccione una opción válida.")
